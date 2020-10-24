b730371d048534f61d74e008b3318e409edcad84
nene <48694062nene@gmail.com>
2020-10-25 03:34:02 +0900

    first commit

---
+++ test/controllers/posts_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class PostsControllerTest < ActionDispatch::IntegrationTest
+  def index  # indexアクションを定義した
+  end
+  
+  # test "the truth" do
+  #   assert true
+  # end
+end
